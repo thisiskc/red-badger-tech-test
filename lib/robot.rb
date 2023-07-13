6bfa40cd69dd298e0a3853d417bd86c0cec0feeb
KC Thomas <thisiskcthomas@gmail.com>
2023-07-13 22:03:19 +0100

    add robot move method

--- lib/robot.rb
+++ lib/robot.rb
@@ -5,4 +5,24 @@ class Robot
       @direction = direction
       @lost = false
     end
+
+    def move(instructions, grid)
+        instructions.each_char do |char|
+          next if @lost
+    
+          case char
+          when 'F'
+            move_forward(grid)
+          when 'R'
+            turn_right
+          when 'L'
+            turn_left
+          end
+        end
+
+        get_final_position
+      end
+    end
+
 end
+

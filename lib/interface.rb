813bcfff3b42397da05a90aae1b347a988ad9cf9
KC Thomas <thisiskcthomas@gmail.com>
2023-07-13 22:52:19 +0100

    parse input

--- lib/interface.rb
+++ lib/interface.rb
@@ -2,5 +2,9 @@ require_relative 'robot'
 require_relative 'grid'
 
 def send_instructions(input)
+  output = []
+  parsed_input = input.split(/\n+/)
+  grid_size = [parsed_input[0][0].to_i, parsed_input[0][2].to_i]
 
+  parsed_instructions = parsed_input[1..].partition.each_with_index { |_el, i| i.even? }
 end
